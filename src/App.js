2ac0eea0441c255e7a351e56c88cc10212223465
“Dmitry Ivakh” <divakh@atncorp.com>
2022-10-10 22:26:22 +0300

    Added 'Post' component and some styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Post from './components/Post';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    this.getPosts();
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { posts: [] };
+    this.getPosts = this.getPosts.bind(this);
+  }
+  postsLink = 'https://jsonplaceholder.typicode.com/posts';
+
+  async sendData(link, method = 'GET', body) {
+    let fetchData = {
+      method: method,
+      body: JSON.stringify(body),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    };
+    if (method === 'GET') {
+      fetchData = {};
+    }
+
+    let response = await fetch(link, fetchData);
+
+    let data = await response.json();
+
+    return data;
+  }
+
+  async getPosts() {
+    let postsList = await this.sendData(this.postsLink);
+
+    this.setState({ posts: postsList });
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        {this.state.posts.map((el) => {
+          return <Post key={el.id} post={el} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
