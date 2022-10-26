79fb99da01e0189dcf7b54839822041b57406fe7
“Dmitry Ivakh” <divakh@atncorp.com>
2022-10-27 00:08:31 +0300

    Implemented deleting and editing post

--- src/App.js
+++ src/App.js
@@ -10,40 +10,70 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { posts: [] };
-    this.getPosts = this.getPosts.bind(this);
-  }
-  postsLink = 'https://jsonplaceholder.typicode.com/posts';
-
-  async sendData(link, method = 'GET', body) {
-    let fetchData = {
-      method: method,
-      body: JSON.stringify(body),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8'
-      }
-    };
-    if (method === 'GET') {
-      fetchData = {};
-    }
-
-    let response = await fetch(link, fetchData);
-
-    let data = await response.json();
-
-    return data;
   }
 
-  async getPosts() {
-    let postsList = await this.sendData(this.postsLink);
+  getPosts = async () => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    let postsList = await response.json();
 
     this.setState({ posts: postsList });
-  }
+  };
+  deletePost = async (id) => {
+    try {
+      let response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+        { method: 'DELETE' }
+      );
+      if (response.ok) {
+        this.setState({
+          posts: this.state.posts.filter((post) => post.id !== id)
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  editPost = async (id, newTitle, newBody) => {
+    try {
+      let response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+        {
+          method: 'PATCH',
+          body: JSON.stringify({
+            title: `${newTitle}`,
+            body: `${newBody}`
+          }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+          }
+        }
+      );
+
+      response.ok &&
+        this.setState({
+          posts: this.state.posts.map((post) =>
+            post.id === id ? { ...post, title: newTitle, body: newBody } : post
+          )
+        });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   render() {
     return (
       <div className='container'>
         {this.state.posts.map((el) => {
-          return <Post key={el.id} post={el} />;
+          return (
+            <Post
+              deletePost={this.deletePost}
+              editPost={this.editPost}
+              key={el.id}
+              post={el}
+            />
+          );
         })}
       </div>
     );
---
+++ src/components/Post.jsx
@@ -0,0 +1,75 @@
+import React, { PureComponent } from 'react';
+import '../Post.css';
+
+class PostList extends PureComponent {
+  state = { isEdit: false, newTitle: '', newBody: '' };
+  async componentDidMount() {}
+
+  editPost = (e) => {
+    if (e.target.dataset.name === 'title') {
+      this.setState({ newTitle: e.target.value });
+    } else if (e.target.dataset.name === 'body') {
+      this.setState({ newBody: e.target.value });
+    }
+  };
+
+  render() {
+    if (!this.state.isEdit) {
+      return (
+        <div className='post'>
+          <h3>{this.props.post.title}</h3>
+          <div>{this.props.post.body}</div>
+          <button
+            onClick={() => {
+              this.setState({ isEdit: true });
+            }}
+          >
+            EDIT
+          </button>
+
+          <i
+            className='fa-sharp fa-solid fa-trash'
+            onClick={() => {
+              this.props.deletePost(this.props.post.id);
+            }}
+          >
+            <span>delete</span>
+          </i>
+        </div>
+      );
+    } else {
+      return (
+        <form className='post'>
+          <p>Title:</p>
+          <textarea
+            data-name='title'
+            defaultValue={this.props.post.title}
+            onInput={this.editPost}
+          />
+          <p>Text:</p>
+          <textarea
+            data-name='body'
+            defaultValue={this.props.post.body}
+            onInput={this.editPost}
+          />
+
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              this.props.editPost(
+                this.props.post.id,
+                this.state.newTitle || this.props.post.title,
+                this.state.newBody || this.props.post.body
+              );
+              this.setState({ isEdit: false });
+            }}
+          >
+            DONE
+          </button>
+        </form>
+      );
+    }
+  }
+}
+
+export default PostList;
